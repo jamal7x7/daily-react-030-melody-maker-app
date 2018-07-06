3b8134d04d0b594755625fb42aff52358fc3a2e3
jamal7X7 <jamal7x7@gmail.com>
2018-07-07 00:27:32 +0100

    just playing with sound

--- src/components/App.js
+++ src/components/App.js
@@ -31,21 +31,24 @@ class MelodyMakerApp extends Component {
   }
 
   handleClick = () => {
-      //create a synth and connect it to the master output (your speakers)
+     
       const synth = new Tone.Synth().toMaster()
-    
-      //play a middle 'C' for the duration of an 8th note
       synth.triggerAttackRelease("C4", "8n")
 
   }
 
   componentDidMount() {
-     
-    //create a synth and connect it to the master output (your speakers)
-    const synth = new Tone.Synth().toMaster()
     
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n")
+    const synth = new Tone.Synth().toMaster()
+   //play a note every quarter-note
+    const loop = new Tone.Loop(function(time){
+      synth.triggerAttackRelease("C2", "8n", time);
+    }, "4n")
+
+    //loop between the first and fourth measures of the Transport's timeline
+    loop.start("1m").stop("4m")
+
+    Tone.Transport.start();
 
   }
 
